cbe80d985d364f0cf6b49b40d6c9ded866b68274
StephenPopovich <stephenpopovich16@gmail.com>
2018-07-24 13:56:57 -0700

    add name form and lesson 1/2 notes

---
+++ test.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="Stephen Popovich">
+
+    <title>Test</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom fonts for this template -->
+    <link href="vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href='https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Merriweather:400,300,300italic,400italic,700,700italic,900,900italic' rel='stylesheet' type='text/css'>
+
+    <!-- Plugin CSS -->
+    <link href="vendor/magnific-popup/magnific-popup.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="css/creative.css" rel="stylesheet">
+
+  </head>
+
+  <body id="page-top">
+
+
+    <?php
+    // Lesson One
+      // $myvar = "this is my variable";
+      // $mynumber = 5;
+      //   $mynumber2 = 3;
+      //   $sum = $mynumber + $mynumber2;
+      //   $name = "Jake";
+      //   echo "Hello, " . $name;
+
+    // Lesson Two
+    //   $loggedIn = false;
+    //
+    //   if ($loggedIn == true) {
+    //     echo "You are logged in";
+    //
+    //   }else{
+    //     echo "Please log in";
+    //   }
+
+
+     ?>
+     <form action="process.php" method="post">
+
+       Enter Your Name <input type="text" name="name" value="">
+       <input type="submit" name="" value="Submit">
+     </form>
+
+
+
+    <!-- Bootstrap core JavaScript -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+    <!-- Plugin JavaScript -->
+    <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+    <script src="vendor/scrollreveal/scrollreveal.min.js"></script>
+    <script src="vendor/magnific-popup/jquery.magnific-popup.min.js"></script>
+
+    <!-- Custom scripts for this template -->
+    <script src="js/creative.min.js"></script>
+
+  </body>
+
+</html>
